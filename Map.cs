b11ed0e997307e5707644c97a3940a522d601bce
Stas2404 <stanislavyudin24@gmail.com>
2025-03-24 19:12:53 +0200

    Map.cs

--- Map.cs
+++ Map.cs
@@ -1,57 +1,60 @@
-using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Курсовий_проект
+internal class Map
 {
-    internal class Map
+    const int Width = 10;
+    const int Height = 10;
+    BaseElement[,] field = new BaseElement[Width, Height];
+    Player player;
+
+    public void Init()
     {
-        const int Width = 10;
-        const int Height = 10;
-        Element[,] field = new Element[Width, Height];
-
-        public void Init() 
+        for (int i = 0; i < Height; i++)
         {
-            for (int i = 0; i < Height; i++)
+            for (int j = 0; j < Width; j++)
             {
-                for (int j = 0; j < Width; j++)
-                {
-                    field[i, j] = new Element("#", Color.White, Color.Black);
-                }
-            }
-            field[0, 0] = new Element("P", Color.Green, Color.Black);
-            CreateElement(5, "W", Color.Blue, Color.Black);
-            CreateElement(10, "E", Color.Red, Color.Black);
-
-        }
-        public void CreateElement(int count, string output, Color foreground, Color background)
-        {
-            Random rnd = new Random();
-            for (int i = 0; i < count; i++)
-            {
-                int x, y;
-                do
-                {
-                    x = rnd.Next(0, Width);
-                    y = rnd.Next(0, Height);
-                } while (field[y, x].Output != "#"); 
-
-                field[y, x] = new Element(output, foreground, background);
+                field[i, j] = new Element("#", Color.White, Color.Black, true);
             }
         }
-        public void Print()
+
+        player = new Player(0, 0);
+        field[0, 0] = player;
+
+        CreateElement(5, "W", Color.Blue, Color.Black, false);
+        CreateElement(10, "E", Color.Red, Color.Black, true);
+    }
+
+    public void CreateElement(int count, string output, Color foreground, Color background, bool isPassable)
+    {
+        Random rnd = new Random();
+        for (int i = 0; i < count; i++)
         {
-            for (int i = 0; i < Height; i++)
+            int x, y;
+            do
             {
-                for (int j = 0; j < Width; j++)
-                {
-                    Console.Write(field[i, j].Output + " ");
-                }
-                Console.WriteLine();
+                x = rnd.Next(0, Width);
+                y = rnd.Next(0, Height);
+            } while (!(field[y, x] is Element) || field[y, x].Output != "#" || (x == player.X && y == player.Y));
+
+            field[y, x] = new Element(output, foreground, background, isPassable);
+        }
+    }
+
+
+    public void MovePlayer(int dx, int dy)
+    {
+        player.Move(dx, dy, Width, Height, field);
+    }
+
+    public void Print()
+    {
+        for (int i = 0; i < Height; i++)
+        {
+            for (int j = 0; j < Width; j++)
+            {
+                Console.Write(field[i, j].Output + " ");
             }
+            Console.WriteLine();
         }
     }
 }
