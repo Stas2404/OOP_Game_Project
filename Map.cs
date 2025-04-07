cebe2b11edc7f4015b946134274f1a0cc8d82e31
Stas2404 <stanislavyudin24@gmail.com>
2025-04-07 22:06:38 +0300

    Map.cs

--- Map.cs
+++ Map.cs
@@ -5,7 +5,9 @@ internal class Map
     const int Width = 10;
     const int Height = 10;
     BaseElement[,] field = new BaseElement[Width, Height];
-    Player player;
+    public Player player;
+    private List<(int x, int y, string output, Color fg, Color bg, bool isPassable)> initialEnemies
+    = new List<(int, int, string, Color, Color, bool)>();
 
     public void Init()
     {
@@ -22,12 +24,16 @@ internal class Map
 
         CreateElement(5, "W", Color.Blue, Color.Black, false);
         CreateElement(10, "E", Color.Red, Color.Black, true);
+
+        player.LevelsHistory.Add(player.Level);
     }
 
     public void CreateElement(int count, string output, Color foreground, Color background, bool isPassable)
     {
         Random rnd = new Random();
-        for (int i = 0; i < count; i++)
+        int enemyNumber = rnd.Next(3, 6);
+
+        for (int i = 0; i < enemyNumber; i++)
         {
             int x, y;
             do
@@ -37,6 +43,8 @@ internal class Map
             } while (!(field[y, x] is Element) || field[y, x].Output != "#" || (x == player.X && y == player.Y));
 
             field[y, x] = new Element(output, foreground, background, isPassable);
+
+            initialEnemies.Add((x, y, output, foreground, background, isPassable));
         }
     }
 
@@ -45,6 +53,26 @@ internal class Map
     {
         player.Move(dx, dy, Width, Height, field);
     }
+
+    public void ResetAfterDefeat()
+    {
+        for (int i = 0; i < Height; i++)
+        {
+            for (int j = 0; j < Width; j++)
+            {
+                field[i, j] = new Element("#", Color.White, Color.Black, true);
+            }
+        }
+
+        player.SetPosition(0, 0);
+        field[0, 0] = player;
+
+        foreach (var (x, y, output, fg, bg, isPassable) in initialEnemies)
+        {
+            field[y, x] = new Element(output, fg, bg, isPassable);
+        }
+    }
+
 
     public void Print()
     {
