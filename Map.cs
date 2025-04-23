40d9a80e6ea1aed218685a0bc6cb624176d76adc
Stas2404 <stanislavyudin24@gmail.com>
2025-04-23 21:49:03 +0300

    Map.cs

--- Map.cs
+++ Map.cs
@@ -1,4 +1,5 @@
 using System.Drawing;
+using System.IO;
 
 internal class Map
 {
@@ -24,6 +25,7 @@ internal class Map
 
         CreateElement(5, "W", Color.Blue, Color.Black, false);
         CreateElement(10, "E", Color.Red, Color.Black, true);
+
 
         player.LevelsHistory.Add(player.Level);
     }
@@ -83,6 +85,63 @@ internal class Map
                 Console.Write(field[i, j].Output + " ");
             }
             Console.WriteLine();
+        }
+    }
+
+    public void SaveGame(string path)
+    {
+        using (StreamWriter writer = new StreamWriter(path))
+        {
+            writer.WriteLine($"{player.X},{player.Y},{player.Level}");
+
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    BaseElement elem = field[y, x];
+
+                    if (elem == player || elem.Output == "#")
+                        continue;
+
+                    writer.WriteLine($"{x},{y},{elem.Output},{(int)elem.Forecolor.ToArgb()},{(int)elem.Backcolor.ToArgb()},{elem.IsPassable}");
+                }
+            }
+        }
+    }
+
+    public void LoadGame(string path)
+    {
+        if (!File.Exists(path)) return;
+
+        var lines = File.ReadAllLines(path);
+
+        string[] playerData = lines[0].Split(',');
+        int playerX = int.Parse(playerData[0]);
+        int playerY = int.Parse(playerData[1]);
+        int playerLevel = int.Parse(playerData[2]);
+
+        for (int i = 0; i < Height; i++)
+            for (int j = 0; j < Width; j++)
+                field[i, j] = new Element("#", Color.White, Color.Black, true);
+
+        player.SetPosition(playerX, playerY);
+        player.Level = playerLevel;
+        field[playerY, playerX] = player;
+
+        initialEnemies.Clear();
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            string[] parts = lines[i].Split(',');
+            int x = int.Parse(parts[0]);
+            int y = int.Parse(parts[1]);
+            string output = parts[2];
+            Color fg = Color.FromArgb(int.Parse(parts[3]));
+            Color bg = Color.FromArgb(int.Parse(parts[4]));
+            bool isPassable = bool.Parse(parts[5]);
+
+            field[y, x] = new Element(output, fg, bg, isPassable);
+            initialEnemies.Add((x, y, output, fg, bg, isPassable));
         }
     }
 }
