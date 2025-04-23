aeb618c56fea2e06702e6608bcb4a60aa6d456c0
Stas2404 <stanislavyudin24@gmail.com>
2025-04-23 21:50:01 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ internal class Program
         map.Init();
         map.Print();
 
-        List<int> playerLevelsHistory = new List<int>(); // Initialize the list to store player levels
+        List<int> playerLevelsHistory = new List<int>();
 
         while (true)
         {
@@ -27,6 +27,25 @@ internal class Program
                 case ConsoleKey.D:
                     map.MovePlayer(1, 0);
                     break;
+
+                case ConsoleKey.F5:
+                    {
+                        string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "save.txt");
+                        map.SaveGame(path);
+                        Console.WriteLine($"Гру збережено у файл: {path}");
+                        Thread.Sleep(1000);
+                        break;
+                    }
+
+                case ConsoleKey.F6:
+                    {
+                        string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "save.txt");
+                        map.LoadGame(path);
+                        Console.WriteLine($"Гру завантажено з файлу: {path}");
+                        Thread.Sleep(1000);
+                        break;
+                    }
+
                 case ConsoleKey.Escape:
                     return;
             }
