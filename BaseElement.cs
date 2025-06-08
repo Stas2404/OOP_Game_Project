8fc086c3ec6b933365c5752a0a39ba4650c69996
Stas2404 <stanislavyudin24@gmail.com>
2025-06-08 13:08:22 +0300

    BaseElement.cs

--- BaseElement.cs
+++ BaseElement.cs
@@ -1,19 +1,23 @@
 using System.Drawing;
 
-internal abstract class BaseElement
+public abstract class BaseElement
 {
     public string Output { get; set; }
     public Color Forecolor { get; protected set; }
     public Color Backcolor { get; protected set; }
     public bool IsPassable { get; protected set; }
+    public bool IsEnemy { get; protected set; }
 
-    protected BaseElement(string output, Color forecolor, Color backcolor, bool isPassable)
+    protected BaseElement(string output, Color forecolor, Color backcolor, bool isPassable, bool isEnemy = false)
     {
         Output = output;
         Forecolor = forecolor;
         Backcolor = backcolor;
         IsPassable = isPassable;
+        IsEnemy = isEnemy;
     }
 
     public abstract void Print();
+    public abstract void Interact(Player player);
+
 }
