3df89b94100de9820abb0c8606dd9f327e820514
Stas2404 <stanislavyudin24@gmail.com>
2025-06-08 13:12:26 +0300

    Player.cs

--- Player.cs
+++ Player.cs
@@ -1,16 +1,22 @@
+using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Threading;
 
-internal class Player : BaseElement
+public class Player : BaseElement
 {
     public int X { get; private set; }
     public int Y { get; private set; }
-    public int Level { get; set; } = 1;
-    public List<int> LevelsHistory { get; private set; } = new List<int>();
 
-    public Player(int x, int y) : base("P", Color.Green, Color.Black, true)
+    public int Level { get; set; } = 1;
+    public int WinsSinceLastLevel { get; set; } = 0;
+
+    private Map map;
+
+    public Player(int x, int y, Map map) : base("P", Color.Green, Color.Black, true)
     {
-        X = x;
-        Y = y;
+        SetPosition(x, y);
+        this.map = map;
     }
 
     public void SetPosition(int x, int y)
@@ -19,41 +25,50 @@ internal class Player : BaseElement
         Y = y;
     }
 
-
-    public void Move(int dx, int dy, int width, int height, BaseElement[,] field)
+    public void MovePlayer(int dx, int dy)
     {
         int newX = X + dx;
         int newY = Y + dy;
 
-        if (newX >= 0 && newX < width && newY >= 0 && newY < height)
+        if (newX < 0 || newX >= Map.Width || newY < 0 || newY >= Map.Height)
+            return;
+
+        var field = map.Field;
+        var nextElement = field[newY, newX];
+
+        if (!nextElement.IsPassable)
+            return;
+
+        if (nextElement is Enemy enemy)
         {
-            BaseElement nextElement = field[newY, newX];
+            var battle = new Battle(this, enemy);
+            var battleUI = new BattleConsoleUI(battle);
+            battleUI.Run();
 
-            if (!nextElement.IsPassable)
-            {
+            if (!battle.PlayerWon)
                 return;
-            }
 
-            if (nextElement.Output == "E")
-            {
-                LevelsHistory.Add(Level);
-                Battle battle = new Battle(this, (Element)nextElement, LevelsHistory);
+            field[newY, newX] = new EmptyTile();
+        }
 
-                if (!battle.PlayerWon)
-                {
-                    return; 
-                }
-            }
+        field[Y, X] = new EmptyTile();
+        SetPosition(newX, newY);
+        field[Y, X] = this;
 
-            field[Y, X] = new Element("#", Color.White, Color.Black, true);
-            X = newX;
-            Y = newY;
-            field[Y, X] = this;
+        SoundManager.PlayMoveSound();
+
+        if (!map.HasEnemiesLeft())
+        {
+            map.GenerateNextLevel(map.IsCustomLevel);
         }
     }
 
     public override void Print()
     {
         Console.Write(Output);
+    }
+
+    public override void Interact(Player player)
+    {
     }
 }
