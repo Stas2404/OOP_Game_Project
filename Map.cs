f9ee609d4df61abe4abf25f54cfad24730f70524
Stas2404 <stanislavyudin24@gmail.com>
2025-06-27 17:29:59 +0300

    Map.cs

--- Map.cs
+++ Map.cs
@@ -1,18 +1,28 @@
-using System.Drawing;
-using System.IO;
 using System;
 using System.Collections.Generic;
-using System.Threading;
+using System.Drawing;
+using System.IO;
 
 public class Map
 {
-    internal const int Width = 10;
-    internal const int Height = 10;
-    BaseElement[,] field = new BaseElement[Width, Height];
+    public const int Width = 10;
+    public const int Height = 10;
+
+    private readonly IGameUI ui;
+    private BaseElement[,] field = new BaseElement[Height, Width];
+    private readonly object? form;
+    private readonly Game game;
+
     public Player player;
     public GameLevel GameLevel { get; private set; } = new GameLevel();
     public bool IsCustomLevel { get; set; } = false;
 
+    public Map(IGameUI ui, Game game, object? form = null)
+    {
+        this.ui = ui;
+        this.game = game;
+        this.form = form;
+    }
 
     public BaseElement this[int y, int x]
     {
@@ -20,23 +30,26 @@ public class Map
         set => field[y, x] = value;
     }
 
-
-
     public void Init()
     {
-        field = new BaseElement[Width, Height];
+        field = new BaseElement[Height, Width];
 
-        for (int i = 0; i < Height; i++)
-            for (int j = 0; j < Width; j++)
-                field[i, j] = new EmptyTile();
+        for (int y = 0; y < Height; y++)
+            for (int x = 0; x < Width; x++)
+                this[y, x] = new EmptyTile();
 
-        player = new Player(0, 0, this);
-        field[0, 0] = player;
+        player = new Player(0, 0, this, ui, game, form);
+        this[0, 0] = player;
 
+        GenerateObstaclesAndEnemies();
+    }
+
+    private void GenerateObstaclesAndEnemies()
+    {
         for (int i = 0; i < 5; i++)
         {
             var pos = GetRandomFreePosition();
-            field[pos.y, pos.x] = new Wall();
+            this[pos.y, pos.x] = new Wall();
         }
 
         var range = GameLevel.GetEnemyLevelRange();
@@ -48,20 +61,69 @@ public class Map
             var pos = GetRandomFreePosition();
             string sym = (i < tankCount) ? "T" : "E";
             var stats = new EnemyStats(new Random().Next(range.min, range.max + 1), sym == "T");
-            field[pos.y, pos.x] = new Enemy(sym, Color.Red, Color.Black, stats);
+            this[pos.y, pos.x] = new Enemy(sym, Color.Red, Color.Black, stats);
         }
 
         Random rnd = new Random();
-        if (rnd.NextDouble() < 0.2)
-        {
-            var pos = GetRandomFreePosition();
-            field[pos.y, pos.x] = new LevelUpElement();
-        }
 
         if (rnd.NextDouble() < 0.2)
+            this[GetRandomFreePosition().y, GetRandomFreePosition().x] = new LevelUpElement();
+
+        if (rnd.NextDouble() < 0.2)
+            this[GetRandomFreePosition().y, GetRandomFreePosition().x] = new GambleElement();
+    }
+
+    public void GenerateNextLevel(bool isCustomLevel = false)
+    {
+        if (!HasEnemiesLeft())
         {
-            var pos = GetRandomFreePosition();
-            field[pos.y, pos.x] = new GambleElement();
+            if (isCustomLevel)
+            {
+                ShowMessage("🎉 You completed the custom level!");
+                TryInvokeFormMethod("ReturnToMainMenu");
+                return;
+            }
+
+            if (GameLevel.LevelNumber == 5)
+                return;
+
+            GameLevel.IncreaseLevel();
+            field = new BaseElement[Height, Width];
+
+            for (int y = 0; y < Height; y++)
+                for (int x = 0; x < Width; x++)
+                    field[y, x] = new EmptyTile();
+
+            player.SetPosition(0, 0);
+            field[0, 0] = player;
+
+            if (GameLevel.LevelNumber == 2 || GameLevel.LevelNumber == 4)
+            {
+                field = MazeGenerator.GenerateMaze(Width, Height, player, GameLevel);
+            }
+            else if (GameLevel.LevelNumber == 5)
+            {
+                if (player.Level < 5)
+                {
+                    ShowMessage("Your level is too low to fight the boss! Restarting the game.");
+                    GameLevel = new GameLevel();
+                    player.Level = 1;
+                    player.WinsSinceLastLevel = 0;
+                    Init();
+                    return;
+                }
+
+                int bossX = Width / 2;
+                int bossY = Height / 2;
+                var boss = new Enemy("B", Color.DarkRed, Color.Black, new EnemyStats(25));
+                field[bossY, bossX] = boss;
+            }
+            else
+            {
+                GenerateObstaclesAndEnemies();
+            }
+
+            TryInvalidateForm();
         }
     }
 
@@ -74,124 +136,18 @@ public class Map
         {
             x = rnd.Next(0, Width);
             y = rnd.Next(0, Height);
-        }
-        while (!(field[y, x] is EmptyTile) || field[y, x].Output != "." || (x == player.X && y == player.Y));
+        } while (!(this[y, x] is EmptyTile) || this[y, x].Output != "." || (x == player.X && y == player.Y));
 
         return (x, y);
-    }
-
-    public void GenerateNextLevel(bool isCustomLevel = false)
-    {
-        if (!HasEnemiesLeft())
-        {
-            if (isCustomLevel)
-            {
-                Console.WriteLine("You completed the custom level!");
-                Console.WriteLine("Press any key to return to the main menu...");
-                Console.ReadKey();
-
-                Game game = new Game();
-                game.Menu();
-                return;
-            }
-
-            GameLevel.IncreaseLevel();
-
-            field = new BaseElement[Width, Height];
-            for (int i = 0; i < Height; i++)
-                for (int j = 0; j < Width; j++)
-                    field[i, j] = new EmptyTile();
-
-            player.SetPosition(0, 0);
-            field[0, 0] = player;
-
-            Console.WriteLine($"\n--- New game level: {GameLevel.LevelNumber} ---\n");
-            Thread.Sleep(1000);
-
-            if (GameLevel.LevelNumber == 2 || GameLevel.LevelNumber == 4)
-            {
-                field = MazeGenerator.GenerateMaze(Width, Height, player, GameLevel);
-            }
-            else if (GameLevel.LevelNumber == 5)
-            {
-                if (player.Level < 5)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Your level is too low to defeat the boss!");
-                    Console.WriteLine("The game will be restarted...");
-                    Thread.Sleep(3000);
-
-                    GameLevel = new GameLevel();
-                    player.Level = 1;
-                    player.WinsSinceLastLevel = 0;
-
-                    Init();
-                    return;
-                }
-
-                int bossX = Width / 2;
-                int bossY = Height / 2;
-
-                var boss = new Enemy("B", Color.DarkRed, Color.Black, new EnemyStats(25));
-                field[bossY, bossX] = boss;
-            }
-            else
-            {
-                for (int i = 0; i < 5; i++)
-                {
-                    var pos = GetRandomFreePosition();
-                    field[pos.y, pos.x] = new Wall();
-                }
-
-                var range = GameLevel.GetEnemyLevelRange();
-                int totalEnemies = new Random().Next(3, 6);
-                int tankCount = new Random().Next(0, totalEnemies + 1);
-
-                for (int i = 0; i < totalEnemies; i++)
-                {
-                    var pos = GetRandomFreePosition();
-                    string sym = (i < tankCount) ? "T" : "E";
-                    var stats = new EnemyStats(new Random().Next(range.min, range.max + 1), sym == "T");
-                    field[pos.y, pos.x] = new Enemy(sym, Color.Red, Color.Black, stats);
-                }
-
-                Random rnd = new Random();
-                if (rnd.NextDouble() < 0.2)
-                {
-                    var pos = GetRandomFreePosition();
-                    field[pos.y, pos.x] = new LevelUpElement();
-                }
-
-                if (rnd.NextDouble() < 0.2)
-                {
-                    var pos = GetRandomFreePosition();
-                    field[pos.y, pos.x] = new GambleElement();
-                }
-            }
-        }
     }
 
     public bool HasEnemiesLeft()
     {
         foreach (var elem in field)
-        {
             if (elem.IsEnemy && elem.Output != ".")
                 return true;
-        }
+
         return false;
-    }
-
-    public void Print()
-    {
-        for (int i = 0; i < Height; i++)
-        {
-            for (int j = 0; j < Width; j++)
-                Console.Write(field[i, j].Output + " ");
-            Console.WriteLine();
-        }
-
-        Console.WriteLine($"\nPlayer's level: {player.Level}");
-        Console.WriteLine($"Game level: {GameLevel.LevelNumber}");
     }
 
     public void LoadFromFile(string path)
@@ -209,14 +165,14 @@ public class Map
 
         field = new BaseElement[Width, Height];
 
-        for (int i = 0; i < Height; i++)
-            for (int j = 0; j < Width; j++)
-                field[i, j] = new EmptyTile();
+        for (int y = 0; y < Height; y++)
+            for (int x = 0; x < Width; x++)
+                this[y, x] = new EmptyTile();
 
-        player = new Player(playerX, playerY, this);
+        player = new Player(playerX, playerY, this, ui, game, form);
         player.Level = playerLevel;
         player.WinsSinceLastLevel = winsSinceLastLevel;
-        field[playerY, playerX] = player;
+        this[playerY, playerX] = player;
 
         GameLevel = new GameLevel();
         while (GameLevel.LevelNumber < gameLevel)
@@ -233,36 +189,49 @@ public class Map
             Color bg = Color.FromArgb(int.Parse(parts[5]));
             bool isPassable = bool.Parse(parts[6]);
 
-            BaseElement elem;
-
-            switch (type)
+            BaseElement elem = type switch
             {
-                case "Wall":
-                    elem = new Wall();
-                    break;
-                case "Enemy":
-                    int level = parts.Length > 7 ? int.Parse(parts[7]) : GameLevel.GetEnemyLevelRange().min;
-                    bool isTank = parts.Length > 8 && bool.Parse(parts[8]);
-                    var stats = new EnemyStats(level, isTank);
-                    elem = new Enemy(output, fg, bg, stats);
-                    break;
-                case "LevelUpElement":
-                    elem = new LevelUpElement();
-                    break;
-                case "GambleElement":
-                    elem = new GambleElement();
-                    break;
-                default:
-                    elem = new EmptyTile();
-                    break;
-            }
+                "Wall" => new Wall(),
+                "Enemy" => new Enemy(output, fg, bg,
+                    new EnemyStats(int.Parse(parts[7]), bool.Parse(parts[8]))),
+                "LevelUpElement" => new LevelUpElement(),
+                "GambleElement" => new GambleElement(),
+                _ => new EmptyTile()
+            };
 
-            field[y, x] = elem;
+            this[y, x] = elem;
         }
     }
 
     public BaseElement GetElement(int x, int y)
     {
-        return field[y, x];
+        return this[y, x];
+    }
+
+    private void TryInvalidateForm()
+    {
+        if (form == null) return;
+
+        var method = form.GetType().GetMethod("Invalidate", Type.EmptyTypes);
+        method?.Invoke(form, null);
+    }
+
+    private void TryInvokeFormMethod(string methodName)
+    {
+        if (form == null) return;
+
+        var method = form.GetType().GetMethod(methodName, Type.EmptyTypes);
+        method?.Invoke(form, null);
+    }
+
+    private void ShowMessage(string message)
+    {
+        if (form == null)
+            ui.WriteLine(message);
+        else
+        {
+            var method = form.GetType().GetMethod("AppendMessage", new[] { typeof(string) });
+            method?.Invoke(form, new object[] { message });
+        }
     }
 }
