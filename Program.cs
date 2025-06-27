5fa7ed0d1023f0936f419690ae6088b7c396a5cf
Stas2404 <stanislavyudin24@gmail.com>
2025-06-27 17:31:36 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,31 @@
-using System;
-using System.Text;
-
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.OutputEncoding = Encoding.UTF8;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-        Game game = new Game();
-        game.Menu();
+        IGameUI ui = new ConsoleGameUI();
+        GameConsoleUI menu = new GameConsoleUI(ui);
+
+        int choice = menu.ShowMainMenu();
+
+        switch (choice)
+        {
+            case 1:
+                var game = new Game(ui);
+                game.Init();
+                game.Run();
+                break;
+
+            case 2:
+                var editor = new LevelEditor(ui);
+                var editorUI = new LevelEditorConsoleUI(editor, ui);
+                editorUI.Run();
+                break;
+
+            case 3:
+                Environment.Exit(0);
+                break;
+        }
     }
 }
